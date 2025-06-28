6db9fc6ad32d21fc6e0b9ed12c5ac26eec97f059
Marcel <sophia.wegersgmail.com>
2025-06-28 18:48:40 +0200

    First try of input param. Not active right know

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-const base = 1000;
+const base = 1000; // -> Moved as parameter base_input in housing.html
 
 const years = [
   2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023
